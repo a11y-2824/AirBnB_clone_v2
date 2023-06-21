9ba48d6d118f17e8314fe0da18475cf8aff9faa8
a11y-2824 <wachana.anne@gmail.com>
2023-06-22 01:25:28 +0300

    fixed bugs, made files executable

--- models/city.py
+++ models/city.py
@@ -12,8 +12,9 @@ class City(BaseModel, Base):
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',cascade='all, delete, delete-orphan')
-    
+        places = relationship(
+           'Place', backref='cities', cascade='all, delete, delete-orphan')
+
     else:
-        name = ''
-        state_id = ''
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" This is the state Module """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from models import storage_type
 from models.city import City
 from sqlalchemy.orm import relationship
+
+
 class State(BaseModel, Base):
     """ State class and Table Model"""
     __tablename__ = 'states'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',cascade='all, delete, delete-orphan')
+        cities = relationship(
+             'City', backref='state', cascade='all, delete, delete-orphan')
     else:
         name = ''
 
         @property
         def cities(self):
             """
-            returning the list of City instances with state_id == current State.id
-            FileStorage relationship BTWN State and City
+            returns the list of City instances with state_id
+            FileStorage relationship between State and City
             """
             from models import storage
             related = []
